8e57f121ef07a91507ca33967f9e63643499b936
pcsanadi <peter.csanadi.840128@gmail.com>
2019-11-11 14:21:24 +0100

    A: new model: UmpireLevel
    - table name
    - id name
    - no timestamps

---
+++ app/UmpireLevel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UmpireLevel extends Model
+{
+    /**
+     * The table associated with the model
+     * 
+     * @var string 
+     */
+    protected $table = 'umpire_levels';
+
+    /**
+     * The primary key associated with the table.
+     * 
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Indicaes if the IDs are auto-incrementing.
+     * 
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * Indicates if the model should be timestamped.
+     * 
+     * @var bool
+     */
+    public $timestamps = false;
+}
