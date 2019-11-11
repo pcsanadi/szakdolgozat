4820f628990977bae301b479ee0450f67605e411
pcsanadi <peter.csanadi.840128@gmail.com>
2019-11-11 14:21:02 +0100

    A: new model: RefereeLevel
    - table name
    - id name
    - no timestamps

---
+++ app/RefereeLevel.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RefereeLevel extends Model
+{
+    /**
+     * The table associated with the model
+     * 
+     * @var string 
+     */
+    protected $table = 'referee_levels';
+
+    /**
+     * The primary key associated with the table.
+     * 
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Indicaes if the IDs are auto-incrementing.
+     * 
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * Indicates if the model should be timestamped.
+     * 
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var array
+     */
+    protected $fillable = [];
+
+    /**
+     * The attributes that aren't mass assignable
+     * 
+     * @var array
+     */
+    protected $guarded = ['id','level'];
+
+    // public function referees()
+    // {
+    //     return $this->hasMany('App\User');
+    // }
+}
