d393977fd71bfdcc07186b8c9f157a5191b685cb
pcsanadi <peter.csanadi.840128@gmail.com>
2019-11-13 17:56:13 +0100

    M: simplified user views

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -5,30 +5,30 @@
 @endsection
 
 @section('content')
-<div class="container">
+<div class="container justify-content-center">
     <form action="{{route('users')}}" method="POST">
         @csrf
         <div class="form-group row">
-            <label for="name" class="col-sm-2 col-form-label">Név</label>
-            <div class="col-sm-10">
+            <label for="name" class="col-form-label">Név</label>
+            <div class="col">
                 <input type="text" class="form-control" name="name" id="name" placeholder="Név" value=""/>
             </div>
         </div>
         <div class="form-group row">
-            <label for="email" class="col-sm-2 col-form-label">E-mail cím</label>
-            <div class="col-sm-10">
+            <label for="email" class="col-form-label">E-mail cím</label>
+            <div class="col">
                 <input type="email" class="form-control" name="email" id="email" placeholder="E-mail cím" value=""/>
             </div>
         </div>
         <div class="form-group row">
-            <label for="password" class="col-sm-2 col-form-label">Kezdeti jelszó</label>
-            <div class="col-sm-10">
+            <label for="password" class="col-form-label">Kezdeti jelszó</label>
+            <div class="col">
                 <input type="text" class="form-control" name="password" id="password" placeholder="Jelszó" value=""/>
             </div>
         </div>
         <div class="form-group row">
-            <label for="ulevel" class="col-sm-2 col-form-label">Játékvezető szint</label>
-            <div class="col-sm-10">
+            <label for="ulevel" class="col-form-label">Játékvezető szint</label>
+            <div class="col">
                 <select id="ulevel" name="ulevel" class="form-control">
                     <option value="" selected disabled>Válassz...</option>
                     @foreach($umpire_levels as $ulevel)
@@ -38,8 +38,8 @@
             </div>
         </div>
         <div class="form-group row">
-            <label for="rlevel" class="col-sm-2 col-form-label">Döntnök szint</label>
-            <div class="col-sm-10">
+            <label for="rlevel" class="col-form-label">Döntnök szint</label>
+            <div class="col">
                 <select id="rlevel" name="rlevel" class="form-control">
                     <option value="" selected disabled>Válassz...</option>
                     @foreach($referee_levels as $rlevel)
@@ -55,8 +55,12 @@
             </div>
         </div>
         <div class="form-group row">
-            <a href="{{route('users')}}" class="btn btn-outline-info">Mégsem</a>
-            <input type="submit" class="btn btn-outline-info" value="Létrehozás"/>
+            <div class="col">
+                <a href="{{route('users')}}" class="btn">Mégsem</a>
+            </div>
+            <div class="col">
+                <input type="submit" class="btn" value="Létrehozás"/>
+            </div>
         </div>
     </form>
 </div>
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -5,25 +5,25 @@
 @endsection
 
 @section('content')
-<div class="container">
+<div class="container justify-content-center">
     <form action="{{route('users')}}/{{$user->id}}" method="POST">
         @method('PUT')
         @csrf
         <div class="form-group row">
-            <label for="name" class="col-sm-2 col-form-label">Név</label>
-            <div class="col-sm-10">
+            <label for="name" class="col-form-label">Név</label>
+            <div class="col">
                 <input type="text" class="form-control" name="name" id="name" placeholder="Név" value="{{ $user->name }}"/>
             </div>
         </div>
         <div class="form-group row">
-            <label for="email" class="col-sm-2 col-form-label">E-mail cím</label>
-            <div class="col-sm-10">
+            <label for="email" class="col-form-label">E-mail cím</label>
+            <div class="col">
                 <input type="email" class="form-control" name="email" id="email" placeholder="E-mail cím" value="{{ $user->email }}"/>
             </div>
         </div>
         <div class="form-group row">
-            <label for="ulevel" class="col-sm-2 col-form-label">Játékvezető szint</label>
-            <div class="col-sm-10">
+            <label for="ulevel" class="col-form-label">Játékvezető szint</label>
+            <div class="col">
                 <select id="ulevel" name="ulevel" class="form-control">
                     @foreach($umpire_levels as $ulevel)
                         <option value="{{ $ulevel->id }}"
@@ -36,8 +36,8 @@
             </div>
         </div>
         <div class="form-group row">
-            <label for="rlevel" class="col-sm-2 col-form-label">Döntnök szint</label>
-            <div class="col-sm-10">
+            <label for="rlevel" class="col-form-label">Döntnök szint</label>
+            <div class="col">
                 <select id="rlevel" name="rlevel" class="form-control">
                     @foreach($referee_levels as $rlevel)
                         <option value="{{ $rlevel->id }}"
@@ -50,8 +50,8 @@
             </div>
         </div>
         <div class="form-group row">
-            <label for="admin" class="col-sm-2 col-form-label">Admin</label>
-            <div class="col-sm-10">
+            <label for="admin" class="col-form-label">Admin</label>
+            <div class="col">
                 <input id="admin" name="admin" value="admin" class="form-check-input" type="checkbox"
                     @if($user->admin == 1)
                         checked
@@ -60,9 +60,15 @@
             </div>
         </div>
         <div class="form-group row">
-            <input type="reset" class="btn btn-outline-info"/>
-            <a href="{{route('users')}}" class="btn btn-outline-info">Mégsem</a>
-            <input type="submit" class="btn btn-outline-info" value="Mentés"/>
+            <div class="col">
+                <input type="reset" class="btn"/>
+            </div>
+            <div class="col">
+                <a href="{{route('users')}}" class="btn">Mégsem</a>
+            </div>
+            <div class="col">
+                <input type="submit" class="btn" value="Mentés"/>
+            </div>
         </div>
     </form>
 </div>
