932459dd720a31499455e32dd649ad1fd8cd2346
pcsanadi <peter.csanadi.840128@gmail.com>
2019-11-19 23:44:53 +0100

    A: icons in tournament calendar

--- resources/views/tournament/calendar.blade.php
+++ resources/views/tournament/calendar.blade.php
@@ -3,6 +3,7 @@
 @section('header_scripts')
     @parent
     <script src='https://kit.fontawesome.com/a076d05399.js'></script> <!-- Font Awesome 5 check -->
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script> <!-- Font Awesome 5 times -->
 @endsection
 
 @section('content')
@@ -31,17 +32,30 @@
         <div class="yjrb29-table-row">
             <div class="col-2 yjrb29-table-cell">{{ $tournament->date }}</div>
             <div class="col-3 yjrb29-table-cell">{{ $tournament->title }}</div>
-            <div class="col-2 yjrb29-table-cell text-center">
-                {{ $tournament->venue->short_name }}
+            <div class="col-2 yjrb29-table-cell text-center"
+                title="
+{{ $tournament->venue->name }}
+({{$tournament->venue->address}})">
+                 {{ $tournament->venue->short_name }}
             </div>
             <div class="col-1 yjrb29-table-cell text-center">{{ $tournament->requested_umpires }}</div>
-            <div class="col-1 yjrb29-table-cell text-center">{{ $tournament->umpireApplications->count() }}</div>
+            <div class="col-1 yjrb29-table-cell text-center"
+                @if($user->admin)
+                    title="
+@foreach($tournament->umpireApplications as $application)
+{{$application->user->name}}
+@endforeach
+                    "    
+                @endif
+            >{{ $tournament->umpireApplications->count() }}
+            </div>
             @if($user->possible_referee)
                 <div class="col yjrb29-table-cell text-center">
                     @if($tournament->appliedAsReferee)
-                        <a href="#" class="yjrb29-btn-red"
+                        <span class="fas fa-check text-success yjrb29-big"></span>
+                        <a href="#"
                             onclick="event.preventDefault();document.getElementById('delete_referee_{{$tournament->id}}').submit();">
-                            <span class="fas fa-check"></span>
+                            <span class="fas fa-times yjrb29-small text-danger" title="{{ __('Revoke') }}"></span>
                         </a>
                         <form action="{{route('referee',$tournament->id)}}" method="POST" id="delete_referee_{{$tournament->id}}">
                             @method('DELETE')
@@ -49,9 +63,10 @@
                             <input type="hidden" name="filtered" value="{{$filtered}}"/>
                         </form>
                     @elseif(!$filtered)
-                        <a href="#" class="yjrb29-btn-green"
+                        <span class="fas fa-times yjrb29-big text-danger"></span>
+                        <a href="#"
                             onclick="event.preventDefault();document.getElementById('apply_referee_{{$tournament->id}}').submit();">
-                            {{ __('Apply') }}
+                            <span class="fas fa-check yjrb29-small text-success" title="{{ __('Apply') }}"></span>
                         </a>
                         <form action="{{route('referee',$tournament->id)}}" method="POST" id="apply_referee_{{$tournament->id}}">
                             @method('PUT')
@@ -63,9 +78,10 @@
             @if($user->possible_umpire)
                 <div class="col yjrb29-table-cell text-center">
                     @if($tournament->appliedAsUmpire)
-                        <a href="#" class=".text-success" style="color:green"
+                        <span class="fas fa-check text-success yjrb29-big"></span>
+                        <a href="#"
                             onclick="event.preventDefault();document.getElementById('delete_umpire_{{$tournament->id}}').submit();">
-                            <span class="fas fa-check"></span>
+                            <span class="fas fa-times yjrb29-small text-danger" title="{{ __('Revoke') }}"></span>
                         </a>
                         <form action="{{route('umpire',$tournament->id)}}" method="POST" id="delete_umpire_{{$tournament->id}}">
                             @method('DELETE')
@@ -73,9 +89,10 @@
                             <input type="hidden" name="filtered" value="{{$filtered}}"/>
                         </form>
                     @elseif(!$filtered)
-                        <a href="#" class="yjrb29-btn-green"
+                        <span class="fas fa-times yjrb29-big text-danger"></span>
+                        <a href="#"
                             onclick="event.preventDefault();document.getElementById('apply_umpire_{{$tournament->id}}').submit();">
-                            {{ __('Apply') }}
+                            <span class="fas fa-check yjrb29-small text-success" title="{{ __('Apply') }}"></span>
                         </a>
                         <form action="{{route('umpire',$tournament->id)}}" method="POST" id="apply_umpire_{{$tournament->id}}">
                             @method('PUT')
