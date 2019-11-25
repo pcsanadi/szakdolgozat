05e1593490558b80448dc656f3322181b8b182b3
pcsanadi <peter.csanadi.840128@gmail.com>
2019-11-25 20:30:56 +0100

    A: additional tournaments in seeder

--- database/seeds/TournamentsTableSeeder.php
+++ database/seeds/TournamentsTableSeeder.php
@@ -32,6 +32,27 @@ class TournamentsTableSeeder extends Seeder
                 'dateto' => '20200209',
                 'venue_id' => 1,
                 'requested_umpires' => 0
+            ],
+            [
+                'title' => 'Vízmű Kupa U11 U15 U19',
+                'datefrom' => '20190921',
+                'dateto' => '20190921',
+                'venue_id' => 1,
+                'requested_umpires' => 15
+            ],
+            [
+                'title' => 'Senior OB',
+                'datefrom' => '20181124',
+                'dateto' => '20181124',
+                'venue_id' => 3,
+                'requested_umpires' => 0
+            ],
+            [
+                'title' => 'Csapatbajnokság I. 2. forduló',
+                'datefrom' => '20190126',
+                'dateto' => '20190127',
+                'venue_id' => 1,
+                'requested_umpires' => 6
             ]
         ]);
     }
